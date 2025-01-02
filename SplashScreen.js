fa1d71e842bda2941c2ebc9d5d8d6676b28a669e
Chanith Dulsika <159680988+chanithdulsika@users.noreply.github.com>
2025-01-02 12:19:56 +0530

    Add files via upload

---
+++ SplashScreen.js
@@ -0,0 +1,14 @@
+import { View } from "react-native-web"
+
+
+export default function SplashScreen(){
+    return (
+
+    )
+}
+
+const AppLoader = () => {
+    return (
+        <View style = {Styles.container}></View>
+    )
+}
