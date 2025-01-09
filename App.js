a9e0fe4b9367f41f1f729e4856353eeeca84062a
Hiruni Maheesha <hiruni.20231660@iit.ac.lk>
2025-01-09 19:43:58 +0530

    Update App.js

--- App.js
+++ App.js
@@ -4,6 +4,15 @@ import Homepage from './Homepage';
 import React, {useState} from 'react';
 
 export default function App() {
-  
+  return(
+    <View  style = {StyleSheet.container}>
+      <Homepage/>
+      <StatusBar style="auto"/>
+    </View>  
+  );
 }
-
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+  },
+})
