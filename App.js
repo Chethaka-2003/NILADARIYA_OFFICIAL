1fd35ba64a788582f020315a3e0c0e2097e634ab
Hiruni Maheesha <hiruni.20231660@iit.ac.lk>
2025-01-09 19:59:08 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Homepage from './Homepage';
-import React, {useState} from 'react';
+
 
 export default function App() {
   return(
-    <View  style = {StyleSheet.container}>
+    <View  style = {styles.container}>
       <Homepage/>
       <StatusBar style="auto"/>
     </View>  
