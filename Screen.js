c6a65778fe49707af0b62f3005ff55762a36965c
R.M.D. Danidu Pawan Udayakumara <159864340+Danidu-123@users.noreply.github.com>
2025-01-21 12:50:27 +0530

    Update screen

--- Screen.js
+++ Screen.js
@@ -86,7 +86,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    paddingHorizontal: 10,
+    paddingHorizontal: 30,
   },
   logoContainer: {
     alignItems: 'center',
