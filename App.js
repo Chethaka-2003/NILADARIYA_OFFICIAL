679a560afc1c320b6e04b8d7d108a4c3971cb77f
Chanith Dulsika <chanith.20232342@iit.ac.lk>
2025-01-26 03:21:51 +0530

    All update with Settings.js

--- App.js
+++ App.js
@@ -1,28 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import SplashScreen from './SplashScreen';
-import WelcomeScreen from './WelcomePg';
-import Linear from './linear';
+import NavigationBar from './NavigationBar';
 
 export default function App() {
   
-  // return (<WelcomeScreen/>);
-  const [isSplashVisible, setIsSplashVisible] = useState(true);
+  return (<NavigationBar/>);
+  // const [isSplashVisible, setIsSplashVisible] = useState(true);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsSplashVisible(false);
-    }, 4000); // 3 seconds
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     setIsSplashVisible(false);
+  //   }, 4000); // 3 seconds
 
-    return () => clearTimeout(timer);
-  }, []);
+  //   return () => clearTimeout(timer);
+  // }, []);
 
-  return (
-    <View style={{ flex: 1 }}>
-      {isSplashVisible ? <SplashScreen /> : <WelcomeScreen />}
-    </View>
-  );
+  // return (
+  //   <View style={{ flex: 1 }}>
+  //     {isSplashVisible ? <SplashScreen /> : <WelcomeScreen />}
+  //   </View>
+  // );
   }
   
    
--- SplashScreen.js
+++ SplashScreen.js
@@ -1,18 +1,20 @@
 import React from "react";
-import { Image,StyleSheet,View,ImageBackground } from "react-native";
+import { Image,StyleSheet,View,ImageBackground, SafeAreaView } from "react-native";
 import Icon from "../CHIPS_CODE/assets/icon.png";
 import LottieView from "lottie-react-native"
+import Background from "./Background";
 
 export default function SplashScreen(){
     return (
-        <ImageBackground source={require('./assets/Background.jpg')} style={styles.background} resizeMode="cover">
-        <View style = {styles.container}>
-            <View>
-                <Image source={Icon} style={styles.image}/>
-                <LottieView source={require('./assets/Animation - 1735760151144.json')} autoPlay loop style = {styles.animation}/>
-            </View>
-        </View>
-        </ImageBackground>
+        <SafeAreaView style = {{flex:1}}>
+            <Background type = "type1"/>
+                <View style = {styles.container}>
+                    <View>
+                    <Image source={Icon} style={styles.image}/>
+                    <LottieView source={require('./assets/Animation - 1735760151144.json')} autoPlay loop style = {styles.animation}/>
+                    </View>
+                </View>
+        </SafeAreaView>
     )
 }
 
@@ -26,13 +28,6 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',   
     },
-
-    background: {
-        flex: 1, // Makes the background fill the screen
-        justifyContent: 'center', // Centers content vertically
-        alignItems: 'center',
-    },
-
     
     image:{
         width:250,
