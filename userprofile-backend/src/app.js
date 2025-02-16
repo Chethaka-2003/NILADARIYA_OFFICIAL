d58b5fa7e25895585ec7bcd817cd9f332eb1316c
R.M.D. Danidu Pawan Udayakumara <159864340+Danidu-123@users.noreply.github.com>
2025-02-16 17:45:22 +0530

    Update(Backend connect)

--- userprofile-backend/src/app.js
+++ userprofile-backend/src/app.js
@@ -1,22 +1,19 @@
-import express from 'express';
-import connectDB from './config/db';
-import setUserRoutes from './routes/userRoutes';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
+require('dotenv').config();
+const express = require('express');
 const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const connectDB = require('./config/db'); // Import the connectDB function
 
-// Connect to database
+app.use(express.json());
+app.use(cors());
+
 connectDB();
 
-// Middleware
-app.use(express.json());
+app.listen(process.env.PORT, () => {
+  console.log("Local server Started");
+});
 
-// Routes
-setUserRoutes(app);
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.get("/", (req, res) => {
+  res.send({status: "Started"});
 });
