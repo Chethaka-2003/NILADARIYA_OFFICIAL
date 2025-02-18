bfc6da0aaae83fc244f274f80eb7dbb5ead1f226
Savini-Perera <menushi.perera10@gmail.com>
2025-02-18 09:56:18 +0530

    Updated officer backend

--- officer-profile-backend/models/Officer.js
+++ officer-profile-backend/models/Officer.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
+const OfficerSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
+    email: { type: String, unique: true, required: true },
+    phone: { type: String, required: true },
+    position: { type: String },
     password: { type: String, required: true },
-    profilePicture: { type: String, default: "" },
-    isActive: { type: Boolean, default: true },
-}, { timestamps: true });
+});
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model("Officer", OfficerSchema);
