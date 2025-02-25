a9382be0dd62ca1f2204e49c33fea269891af099
R.M.D. Danidu Pawan Udayakumara <159864340+Danidu-123@users.noreply.github.com>
2025-02-25 11:52:33 +0530

    Update

---
+++ CustomLottieAlert.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { Modal, View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import LottieView from 'lottie-react-native';
+import DONE from '../assets/done.json';
+import { useNavigation } from '@react-navigation/native';
+
+const CustomLottieAlert = ({ visible, title, message, onClose, animationSource }) => {
+  const navigation = useNavigation();
+
+  const handleOkPress = () => {
+    onClose();
+    navigation.navigate('LoginScreen');
+  };
+
+  return (
+    <Modal
+      transparent={true}
+      animationType="slide"
+      visible={visible}
+      onRequestClose={onClose}
+    >
+      <View style={styles.overlay}>
+        <View style={styles.alertBox}>
+          <LottieView
+            source={DONE}
+            autoPlay
+            loop
+            style={styles.animation}
+          />
+          <Text style={styles.title}>{title}</Text>
+          <Text style={styles.message}>{message}</Text>
+          <TouchableOpacity style={styles.button} onPress={handleOkPress}>
+            <Text style={styles.buttonText}>OK</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  overlay: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  alertBox: {
+    width: 300,
+    padding: 20,
+    backgroundColor: 'white',
+    borderRadius: 20, // Change border radius here
+    alignItems: 'center',
+  },
+  animation: {
+    width: 150,
+    height: 150,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  message: {
+    fontSize: 16,
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  button: {
+    backgroundColor: 'black',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+});
+
+export default CustomLottieAlert;
---
+++ LoginScreen.js
@@ -0,0 +1,141 @@
+import React, { useState } from 'react';
+import { View, Text, Image, TextInput, StyleSheet, TouchableOpacity, Alert} from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import NavigationBar from '../NavigationBar';
+import axios from 'axios';
+
+
+export default function LoginScreen() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+
+  const navigation = useNavigation();
+
+  const handleLogin = () => {
+    if (!email || !password) {
+      Alert.alert('Please enter email and password');
+      return;
+    } 
+
+    axios.post('http://192.168.8.101:4000/login', { email, password })
+    .then(res => {
+      if (res.data.status === 'OK') {
+        Alert.alert('Success', 'Logged in successfully');
+        navigation.navigate(NavigationBar);
+      } else {
+        Alert.alert('Error', res.data.message);
+      }
+    })
+    .catch(error => {
+      Alert.alert('Error', error.message);
+    });
+  }
+
+
+  return (
+    <View style={styles.container}>
+      <Image source={require('../assets/login 1.jpg')} style={styles.backgroundImage} />
+      <Image source={require('../assets/Logo.png')} style={styles.logo} />
+      <View style={styles.inputContainer}>
+        <TextInput 
+          style={styles.input} 
+          placeholder="Username" 
+          placeholderTextColor="black"
+        />
+         <View style={styles.passwordContainer}>
+          <TextInput 
+            style={styles.input} 
+            placeholder="Password" 
+            placeholderTextColor="black" 
+            secureTextEntry={!showPassword} 
+            value={password} 
+            onChangeText={setPassword}
+          />
+          <TouchableOpacity 
+            onPress={() => setShowPassword(!showPassword)} 
+            style={styles.showPasswordIcon}
+          >
+            <MaterialIcons 
+              name={showPassword ? 'visibility' : 'visibility-off'} 
+              size={24} 
+              color="black" 
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+      <TouchableOpacity onPress={() => alert('Forgot Password functionality')}>
+        <Text style={styles.forgotPassword}>Forgot Password?</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.loginButton} onPress = {handleLogin}>
+        <Text style={styles.loginButtonText}>Log In</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  backgroundImage: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+  },
+  text: {
+    fontSize: 24,
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  logo: {
+    width: 200
+    , // Adjust the width as needed
+    height: 100, // Adjust the height as needed
+    marginTop: 50, // Space from the top of the window
+    position: 'absolute', // Ensures it's placed above the background
+    top: 50, // Positions the logo near the top
+    alignSelf: 'center', // Centers the logo horizontally
+  },
+  inputContainer: {
+    width: '80%',
+    marginTop: 420, // Adjust this based on your layout
+  },
+  input: {
+    height: 50,
+    backgroundColor: '#d3d3d3',
+    borderRadius: 20,
+    marginBottom: 30,
+    paddingHorizontal: 15,
+    fontSize: 16,
+    color: '#333',
+  },
+  forgotPassword: {
+    marginTop: 30,
+    color: 'black',
+    fontSize: 14,
+    textAlign: 'center',
+    textDecorationLine: 'underline',
+  },
+  loginButtonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  loginButton: {
+    marginTop: 50,
+    backgroundColor: 'black',
+    paddingVertical: 15,
+    paddingHorizontal: 50,
+    borderRadius: 410,
+    width: 200,
+  },
+  showPasswordIcon: {
+    marginLeft: 280,
+    marginTop: -65,
+  },
+});
