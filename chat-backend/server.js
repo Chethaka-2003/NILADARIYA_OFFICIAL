689caf354be235f099f3ff3bdfdd4ce1ac5e2376
R.M.D. Danidu Pawan Udayakumara <159864340+Danidu-123@users.noreply.github.com>
2025-03-17 12:51:03 +0530

    update

--- chat-backend/server.js
+++ chat-backend/server.js
@@ -3,6 +3,10 @@ const http = require('http');
 const socketIo = require('socket.io');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config({ path: './.env' });
 
 // Create an Express app
 const app = express();
@@ -14,16 +18,21 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/chat', {
+const mongouri = process.env.MONGO_URI;
+mongoose.connect(mongouri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.log(err));
 
 // Define a message schema and model
 const messageSchema = new mongoose.Schema({
   text: String,
   sender: String,
   timestamp: { type: Date, default: Date.now },
+},{
+  collection: 'message'
 });
 
 const Message = mongoose.model('Message', messageSchema);
@@ -33,7 +42,7 @@ io.on('connection', (socket) => {
   console.log('New client connected');
 
   // Send all messages to the client
-  Message.find().then((messages) => {
+  Message.find().then((messages) => { 
     socket.emit('initialMessages', messages);
   });
 
@@ -56,3 +65,4 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
