b8f0d0efe3cfcbe56fb012897e1fe390a2651f0d
Your Name <your.email@example.com>
2025-03-23 14:36:05 +0530

    eddited

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+ 
+app.get('/', (req, res) => {
+
+  res.send('Hello from Express on Google Cloud Run!');
+
+});
+ 
+app.listen(PORT, () => {
+
+  console.log(`Server is running on port ${PORT}`);
+
+});
+
+ 
